568faf94a50f5a03e74f36abb3a39e9528190b4f
Aichuk Tripura <living.being123@gmail.com>
2022-03-25 01:50:42 -0400

    Added update and delete to transaction table

--- transactions.py
+++ transactions.py
@@ -7,7 +7,7 @@ class transactions:
         con = sqlite3.connect(db)
         cur = con.cursor()
         # itemNo becomes an alias of rowid column
-        cur.execute('''CREATE TABLE IF NOT EXISTS Transactions(
+        cur.execute('''CREATE TABLE IF NOT EXISTS transactions(
                     itemNo INTEGER PRIMARY KEY, 
                     amount float, 
                     category text, 
@@ -18,9 +18,14 @@ class transactions:
         con.close()
         self.db = db
 
-    def select_one(self):
+    def select_one(self, itemNo):
         con = sqlite3.connect(self.db)
         cur = con.cursor()
+        cur.execute("SELECT * from transactions where itemNo=(?)", (itemNo))
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return to_cat_dict(tuples[0])
 
     # retrieves all transactions
     def select_all(self):
@@ -31,12 +36,12 @@ class transactions:
         con.commit()
         con.close()
 
-    '''Adds transactions'''
-
+    # Adds transaction
     def add(self, item):
-        con= sqlite3.connect(self.db)
+        con = sqlite3.connect(self.db)
         cur = con.cursor()
-        cur.execute("INSERT INTO transactions VALUES(?,?,?,?,?)",(item['itemNo'],item['amount'] ,item['category_t'],item['date'],item['description']))
+        cur.execute("INSERT INTO transactions VALUES(?,?,?,?,?)",
+                    (item['itemNo'], item['amount'], item['category_t'], item['date'], item['description']))
         con.commit()
         cur.execute("SELECT last_insert_rowid()")
         last_rowid = cur.fetchone()
@@ -44,3 +49,12 @@ class transactions:
         con.close()
         return last_rowid[0]
 
+    # Deletes transaction
+    def delete(self, itemNo):
+        con = sqlite3.connect(self.dbfile)
+        cur = con.cursor()
+        cur.execute('''DELETE FROM transactions
+                       WHERE itemNo=(?);
+        ''', (itemNo))
+        con.commit()
+        con.close()
