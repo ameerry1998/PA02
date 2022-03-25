dfbbd1661783c42ad36ff1498f8bb7317c9b5380
Aichuk Tripura <living.being123@gmail.com>
2022-03-25 01:32:30 -0400

    More changes

--- transactions.py
+++ transactions.py
@@ -6,15 +6,24 @@ class transactions:
     def __init__(self, db):
         con = sqlite3.connect(db)
         cur = con.cursor()
-        cur.execute('''CREATE TABLE IF NOT EXISTS transactions
-                    (itemNo INT, amount FLOAT, category_t TEXT, date DATE, description TEXT)''')
+        # itemNo becomes an alias of rowid column
+        cur.execute('''CREATE TABLE IF NOT EXISTS Transactions(
+                    itemNo INTEGER PRIMARY KEY, 
+                    amount float, 
+                    category text, 
+                    date date, 
+                    description text
+                    )''')
         con.commit()
         con.close()
         self.db = db
 
-    '''returns all the transactions'''
+    def select_one(self):
+        con = sqlite3.connect(self.db)
+        cur = con.cursor()
 
-    def show_trans(self):
+    # retrieves all transactions
+    def select_all(self):
         con = sqlite3.connect(self.db)
         cur = con.cursor()
         cur.execute("SELECT * from transactions")
