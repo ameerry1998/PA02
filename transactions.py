e8adfd724756a3871d8b445c7abea6056370287d
Aichuk Tripura <living.being123@gmail.com>
2022-03-24 23:07:02 -0400

    Modified transactions.py

--- transactions.py
+++ transactions.py
@@ -1,17 +1,19 @@
 import sqlite3
 import csv
 
+
 class transactions:
     def __init__(self, db):
         con = sqlite3.connect(db)
         cur = con.cursor()
         cur.execute('''CREATE TABLE IF NOT EXISTS transactions
-                    (NumofItem INT, amount FLOAT, category_t TEXT, date DATE, description TEXT)''')
+                    (itemNo INT, amount FLOAT, category_t TEXT, date DATE, description TEXT)''')
         con.commit()
         con.close()
         self.db = db
 
     '''returns all the transactions'''
+
     def show_trans(self):
         con = sqlite3.connect(self.db)
         cur = con.cursor()
